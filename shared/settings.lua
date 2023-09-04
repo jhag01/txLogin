2426c0956e1b69137b71cfc861eea8fda56f7726
Joost H <26064691+jhag01@users.noreply.github.com>
2023-09-04 18:10:47 +0200

    add shared settings

---
+++ shared/settings.lua
@@ -0,0 +1,24 @@
+Settings = {
+
+    Command = 'txLogin',
+    AcePerms = true,
+
+    Notify = 'none',
+
+    Debug = false,
+
+    EnableLogs = false,
+    Logger = 'none',
+
+    DiscordLogs = {
+        Webhook = 'WEBHOOK',
+        Username = 'txLogin',
+        Color = '16711680',
+        Title = 'txLogin:Logger',
+        Footer = 'githubUser: jhag01'
+    },
+
+    oxLogs = {
+        Event = 'txLogin'
+    }
+}
---
+++ shared/strings.lua
@@ -0,0 +1,13 @@
+Strings = {
+    
+    MenuEnable = 'Your txMenu has been enabled!',
+    MenuDisable = 'Your txMenu has been disabled!',
+
+    Logs = 'Admin changed their menu access to: ',
+
+    debugStateChange = 'state changed: ',
+    debugLogTriggered = 'txLogin:Logger triggered',
+    debugNoLog = 'no log initiated',
+    debugNoPerms = 'no perms found',
+    
+}

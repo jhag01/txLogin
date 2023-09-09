2fdc9f72b6487a0224279cfbdf91849cd37baed5
Joost H <26064691+jhag01@users.noreply.github.com>
2023-09-09 02:50:03 +0200

    rearrange logging

--- server/logger.lua
+++ server/logger.lua
@@ -1,3 +1,29 @@
+function oxLog(source, txuser, txActive)
+    lib.logger(source, Settings.oxLogs.Event, txUser .. ' | ID: ' .. source .. ' | ' .. Strings.Logs '' .. txActive)
+end
+
+function discordLog(source, txuser, txActive)
+
+    local dcLog = Settings.DiscordLogs
+
+    local Embed = {
+        ["color"] = dcLog.Color,
+        ["type"] = "rich",
+        ["title"] = dcLog.title,
+        ["description"] = txUser .. ' | ID: ' .. source .. ' | ' .. Strings.Logs .. txActive,
+        ["footer"] = {
+        ["text"] = dcLog.Footer .. ' | ' .. os.date('%c')
+        }
+    }
+
+    PerformHttpRequest(dcLog.Webhook, function(err, text, headers) end, 'POST', json.encode({ username = log.Username, embeds = { Embed } }), { ['Content-Type'] = 'application/json' })
+
+end
+
+function customLog(source, txActive, txUser)
+    -- insert own logging system
+end
+
 RegisterServerEvent('txLogin:Logger')
 AddEventHandler('txLogin:Logger', function(txActive, txUser)
 
